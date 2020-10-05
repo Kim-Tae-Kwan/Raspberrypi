42277bfea97023a5ecb6f848282aa9ef6a9be5a4
Kim-Tae-Kwan <vodjaos@gmail.com>
2020-10-05 15:58:17 +0900

    파이널 최종

--- Test_Fire.py
+++ Test_Fire.py
@@ -21,12 +21,12 @@ I2Cbus = smbus.SMBus(1)
 SlaveAddress = AVR_1_ADDRESS
 dev_id = "TKKim"
 #mqtt publisher
-broker_address = "210.119.12.60"
+broker_address = "210.119.12.77"
 client2 = mqtt.Client(dev_id)
 client2.connect(broker_address)#접속
 
 def I2C_Read_data(Data_Name):    
-
+    out_str=''
     for i in range(16):
         in_data = I2Cbus.read_byte_data(SlaveAddress, Data_Name)
         if in_data == 13: 
@@ -39,35 +39,43 @@ def I2C_Read_data(Data_Name):
 #dh11 init
 while True:
     count += 1        
-    currtime = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S') # 현재 time 생성
+    #currtime = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S') # 현재 time 생성
     
     # groupdata 만들기
     raw_data = OrderedDict() #데이터 생성
-    raw_data["Curr_time"] = currtime
+    #raw_data["Curr_time"] = currtime
     
     try:
         raw_data["Fire"] = I2C_Read_data(Fire_Data)
+        print(raw_data["Fire"])
         raw_data["Water"] = I2C_Read_data(Water_Data)
+        print(raw_data["Water"])
         raw_data["Photo"] = I2C_Read_data(Photo_Data)
+        print(raw_data["Photo"])
         
-        split_TempHumi1 = I2C_Read_data(Floor1_Data).split('/')
-        raw_data["firsttemp"] = split_TempHumi1[0]
-        raw_data["firsthumid"] = split_TempHumi1[1]
+        #split_TempHumi1 = I2C_Read_data(Floor1_Data)
+        #print(split_TempHumi1)
+        #print(split_TempHumi1)
+        #raw_data["firsttemp"] = split_TempHumi1[0]
+        #raw_data["firsthumid"] = split_TempHumi1[1]
 
-        split_TempHumi2 = I2C_Read_data(Floor2_Data).split('/')
-        raw_data["secondtemp"] = split_TempHumi2[0]
-        raw_data["secondhumid"] = split_TempHumi2[1]
+        #split_TempHumi2 = I2C_Read_data(Floor2_Data)
+        #print(split_TempHumi2)
+        #raw_data["secondtemp"] = split_TempHumi2[0]
+        #raw_data["secondhumid"] = split_TempHumi2[1]
 
-        split_TempHumi3 = I2C_Read_data(Floor3_Data).split('/')
-        raw_data["thirdtemp"] = split_TempHumi3[0]
-        raw_data["thirdhumid"] = split_TempHumi3[1]
-                     
-        pub_data = json.dumps(raw_data, ensure_ascii=False, indent="\t") #데이터 json으로 변환
+        #split_TempHumi3 = I2C_Read_data(Floor3_Data).split('/')
+        #raw_data["thirdtemp"] = split_TempHumi3[0]
+        #raw_data["thirdhumid"] = split_TempHumi3[1]
+        
+        
+               
+        #pub_data = json.dumps(raw_data, ensure_ascii=False, indent="\t") #데이터 json으로 변환
         #mqtt published
-        print(count, pub_data)
-        client2.publish("home/device/data/", pub_data) # 데이터 보내기
+        #print(count, pub_data)
+        #client2.publish("Temp_Humid", pub_data) # 데이터 보내기
         
-        #time.sleep(0.1)
+        time.sleep(1)
     except Exception as ex:
         print('Error raised : ',ex)
 
--- i2c_mqtt_sub.py
+++ i2c_mqtt_sub.py
@@ -99,7 +99,7 @@ def on_message(client, userdata, msg):
 
     # 수신한 문자열을 저장할 변수
             out_str = ''
-    elif(str(msg.payload.decode("utf-8"))=='On'):
+    elif(str(msg.payload.decode("utf-8"))=='Fan_On'):
         SlaveAddress = AVR_2_ADDRESS
         out_data = 'r'
         bytes_ch = ConvertStringToBytes(out_data)
@@ -111,8 +111,7 @@ def on_message(client, userdata, msg):
 
     # 수신한 문자열을 저장할 변수
             out_str = ''
-
-    elif(str(msg.payload.decode("utf-8"))=='Off'):
+    elif(str(msg.payload.decode("utf-8"))=='Fan_Off'):
         SlaveAddress = AVR_2_ADDRESS
         out_data = 'v'
         bytes_ch = ConvertStringToBytes(out_data)
@@ -123,25 +122,100 @@ def on_message(client, userdata, msg):
             time.sleep(0.01)
 
     # 수신한 문자열을 저장할 변수
+            out_str = ''        
+    elif(str(msg.payload.decode("utf-8"))=='Fire_On'):
+        SlaveAddress = AVR_2_ADDRESS
+        out_data = 'y'
+        bytes_ch = ConvertStringToBytes(out_data)
+        for i in range(len(bytes_ch)):
+    # 문자(Byte)를 Slave에 전송 한다.
+    # cmd = 0xa0 : 이 코드를 Slave에서 문자 수신 명령으로 해석 한다.
+            I2Cbus.write_byte_data(SlaveAddress, 0xa0, ord(out_data))
+            time.sleep(0.01)
+
+    # 수신한 문자열을 저장할 변수
             out_str = ''
+    elif(str(msg.payload.decode("utf-8"))=='Fire_Off'):
+        SlaveAddress = AVR_2_ADDRESS
+        out_data = 'n'
+        bytes_ch = ConvertStringToBytes(out_data)
+        for i in range(len(bytes_ch)):
+    # 문자(Byte)를 Slave에 전송 한다.
+    # cmd = 0xa0 : 이 코드를 Slave에서 문자 수신 명령으로 해석 한다.
+            I2Cbus.write_byte_data(SlaveAddress, 0xa0, ord(out_data))
+            time.sleep(0.01)
+
+    # 수신한 문자열을 저장할 변수
+            out_str = ''
+    
+    elif(str(msg.payload.decode("utf-8"))=='Detection_On'):
+        SlaveAddress = AVR_2_ADDRESS
+        out_data = 'o'
+        bytes_ch = ConvertStringToBytes(out_data)
+        for i in range(len(bytes_ch)):
+    # 문자(Byte)를 Slave에 전송 한다.
+    # cmd = 0xa0 : 이 코드를 Slave에서 문자 수신 명령으로 해석 한다.
+            I2Cbus.write_byte_data(SlaveAddress, 0xa0, ord(out_data))
+            time.sleep(0.01)
+
+    # 수신한 문자열을 저장할 변수
+            out_str = ''
+    elif(str(msg.payload.decode("utf-8"))=='Detection_Off'):
+        SlaveAddress = AVR_2_ADDRESS
+        out_data = 'p'
+        bytes_ch = ConvertStringToBytes(out_data)
+        for i in range(len(bytes_ch)):
+    # 문자(Byte)를 Slave에 전송 한다.
+    # cmd = 0xa0 : 이 코드를 Slave에서 문자 수신 명령으로 해석 한다.
+            I2Cbus.write_byte_data(SlaveAddress, 0xa0, ord(out_data))
+            time.sleep(0.01)
+
+    # 수신한 문자열을 저장할 변수
+            out_str = ''
+    elif(str(msg.payload.decode("utf-8"))=='Water_On'):
+        SlaveAddress = AVR_2_ADDRESS
+        out_data = 't'
+        bytes_ch = ConvertStringToBytes(out_data)
+        for i in range(len(bytes_ch)):
+    # 문자(Byte)를 Slave에 전송 한다.
+    # cmd = 0xa0 : 이 코드를 Slave에서 문자 수신 명령으로 해석 한다.
+            I2Cbus.write_byte_data(SlaveAddress, 0xa0, ord(out_data))
+            time.sleep(0.01)
+
+    # 수신한 문자열을 저장할 변수
+            out_str = ''
+    elif(str(msg.payload.decode("utf-8"))=='Water_Off'):
+        SlaveAddress = AVR_2_ADDRESS
+        out_data = 'b'
+        bytes_ch = ConvertStringToBytes(out_data)
+        for i in range(len(bytes_ch)):
+    # 문자(Byte)를 Slave에 전송 한다.
+    # cmd = 0xa0 : 이 코드를 Slave에서 문자 수신 명령으로 해석 한다.
+            I2Cbus.write_byte_data(SlaveAddress, 0xa0, ord(out_data))
+            time.sleep(0.01)
+
+    # 수신한 문자열을 저장할 변수
+            out_str = ''                
 try:
     # 새로운 클라이언트 생성
     client = mqtt.Client()
-    client2 = mqtt.Client()
+   # client2 = mqtt.Client()
+    #client.message_callback_add("$SmartBuilding/Led/",on_message)
+    #client.message_callback_add("$SmartBuilding/Fan/",on_message2)
     # 콜백 함수 설정 on_connect(브로커에 접속), on_disconnect(브로커에 접속중료), on_subscribe(topic 구독),
     client.on_connect = on_connect
     client.on_message = on_message
-    client.connect('210.119.12.60',1883)
+    client.connect('210.119.12.77',1883)
 
-    client2.on_connect = on_connect
-    client2.on_message = on_message
-    client2.connect('210.119.12.60',1883)
+   # client2.on_connect = on_connect2
+   # client2.on_message = on_message2
+   # client2.connect('210.119.12.77',1883)
+
+
     # common topic 으로 메세지 발행
-    client.subscribe('SmartBuilding/Led/', 1)
+    client.subscribe([('SmartBuilding/Led/', 1),('SmartBuilding/Fan/',1),('SmartBuilding/Water/',1),('SmartBuilding/Fire/',1),('SmartBuilding/Detection/',1)])
     client.loop_forever()
 
-    client2.subscribe('SmartBuilding/Fan/', 1)
-    client2.loop_forever()
 finally:
     GPIO.cleanup()
 
